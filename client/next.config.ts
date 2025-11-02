9abbc2f1a26f9cc01588a15fd3e8f1ec8c73cb63
AashmanBKT <aashmangupta886@gmail.com>
2025-11-02 12:58:25 +0530

    Update next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -5,6 +5,10 @@ const nextConfig: NextConfig = {
     // This will allow your project to build even if it has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    // !! This disables TypeScript errors !!
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
